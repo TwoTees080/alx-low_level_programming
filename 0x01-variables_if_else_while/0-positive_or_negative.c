efc3b835fe19b7cb37006468afeb0c128265c0b5
TwoTees080 <twotees080@gmail.com>
2023-03-18 14:41:14 -0700

    removed trailing white space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* function to check if number is positive or negative */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
