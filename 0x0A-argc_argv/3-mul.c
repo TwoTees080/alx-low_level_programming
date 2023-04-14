c78cff1b2cfd0066d4361ec958d4e6688858c8df
TwoTees080 <twotees080@gmail.com>
2023-04-14 15:05:16 +0000

    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

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the result of multiplication
+ * @argc: arguments count
+ * @argv: array of strings of arguments passed to main
+ *
+ * Return: on success, result of multiplication
+ * print error and return 1 if program does not receive 2 arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if ((argc - 1) != 2)
+	{
+		printf("%s\n", "Error");
+
+		return (1);
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", mul);
+
+	return (0);
+}
