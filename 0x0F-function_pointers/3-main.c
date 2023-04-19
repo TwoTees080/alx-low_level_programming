b12aab4eeba4021986c4ab43fa6362dfc4975261
TwoTees080 <twotees080@gmail.com>
2023-04-19 21:35:01 +0000

    error fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,32 +12,34 @@
 
 int main(int __attribute__((__unused__)) argc, char **argv)
 {
-	char temp = argv[2][0];
-	int a = 0, num1 = 0, num2 = 0;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (temp != '+' && temp != '-' && temp != '*'
-	     && temp != '/' && temp != '%')
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (argv[2][1] != '\0')
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	 num1 = atoi(argv[1]);
-	 num2 = atoi(argv[3]);
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-	a = (get_op_func(argv[2]))(num1, num2);
-
-	printf("%d\n", a);
 	return (0);
 }
 
