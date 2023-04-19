6d259d7927c121a4ff60afae57ef38385abfc6f7
TwoTees080 <twotees080@gmail.com>
2023-04-19 21:31:25 +0000

    .

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,15 +18,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (i < 5)
-	{
-		if (*s == *(ops[i].op) && *(s + 1) == '\0')
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+
+	return (ops[i].f);
 }
